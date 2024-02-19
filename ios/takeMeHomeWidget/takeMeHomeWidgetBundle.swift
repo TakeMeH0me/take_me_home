c41e1e42276b492beb4c387eb0d3729332380683
CedricHommann <80926047+CedricHommann@users.noreply.github.com>
2024-02-19 10:31:34 +0100

    ios widget added

---
+++ ios/takeMeHomeWidget/takeMeHomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  takeMeHomeWidgetBundle.swift
+//  takeMeHomeWidget
+//
+//  Created by Cedric Hommann on 19.02.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct takeMeHomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        takeMeHomeWidget()
+    }
+}
