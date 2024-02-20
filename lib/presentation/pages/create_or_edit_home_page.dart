36f402643ceb2acb76d7edcfbddf66ce4a148183
jonastnl <138850177+jonastnl@users.noreply.github.com>
2024-02-20 13:02:49 +0100

    Added Home-Input Page (UI)

--- lib/presentation/pages/create_or_edit_home_page.dart
+++ lib/presentation/pages/create_or_edit_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:take_me_home/presentation/router/app_router.dart';
 
 /// A home can be created ([isEditing] = false) or edited ([isEditing] = true) with this page.
 ///
@@ -16,14 +17,124 @@ class CreateOrEditHomePage extends StatefulWidget {
 }
 
 class _CreateOrEditHomePageState extends State<CreateOrEditHomePage> {
+  final TextEditingController _homeNameController = TextEditingController();
+  final TextEditingController _streetController = TextEditingController();
+  final TextEditingController _streetNumberController = TextEditingController();
+  final TextEditingController _postcodeController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+
+    //instead of onChanged in Textfield
+    _homeNameController.addListener(_onHomeNameChanged);
+    _streetController.addListener(_onStreetChanged);
+    _streetNumberController.addListener(_onNumberChanged);
+    _postcodeController.addListener(_onPostcodeChanged);
+    _cityController.addListener(_onCityChanged);
+  }
+
+  void dispose() {
+    _homeNameController.dispose();
+    _streetController.dispose();
+    _streetNumberController.dispose();
+    _postcodeController.dispose();
+    _cityController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text('Create Or Edit Home Page'),
-        ),
+        child: Column(children: [
+          const Text('Create Or Edit Home Page'),
+          Scrollbar(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 10.0),
+                    TextField(
+                      controller: _homeNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Home Name',
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    TextField(
+                      controller: _streetController,
+                      decoration: const InputDecoration(
+                        labelText: 'Street',
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    TextField(
+                      controller: _streetNumberController,
+                      decoration: const InputDecoration(
+                        labelText: 'Street Number',
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    TextField(
+                      controller: _postcodeController,
+                      decoration: const InputDecoration(
+                        labelText: 'Postcode',
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
+      bottomNavigationBar: _buildBottomNavigationBar(context),
     );
   }
+
+  _onHomeNameChanged() {
+    setState() {
+      String homeName = _homeNameController.text;
+    }
+  }
+
+  _onStreetChanged() {
+    setState() {
+      String Street = _streetController.text;
+    }
+  }
+
+  _onNumberChanged() {
+    setState() {
+      String Number = _streetNumberController.text;
+    }
+  }
+
+  _onPostcodeChanged() {
+    setState() {
+      String Postcode = _postcodeController.text;
+    }
+  }
+
+  _onCityChanged() {
+    setState() {
+      String City = _cityController.text;
+    }
+  }
+}
+
+Widget _buildBottomNavigationBar(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: const Icon(Icons.save)),
+  );
 }
