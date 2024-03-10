c18f8cd55b378050ab45ba204db7c0568fbc5e3d
jonastnl <138850177+jonastnl@users.noreply.github.com>
2024-03-10 12:28:04 +0100

    deleted import

--- lib/presentation/pages/create_or_edit_home_page.dart
+++ lib/presentation/pages/create_or_edit_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:take_me_home/presentation/router/app_router.dart';
 
 /// A home can be created ([isEditing] = false) or edited ([isEditing] = true) with this page.
 ///
--- lib/presentation/pages/show_homes_page.dart
+++ lib/presentation/pages/show_homes_page.dart
@@ -12,35 +12,6 @@ class ShowHomesPage extends StatefulWidget {
 class _ShowHomesPageState extends State<ShowHomesPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CurrentLocationCard(
-            leadingIcon: Icon(Icons.gps_fixed),
-            startStation: "Gera - Duale Dochschule",
-            distance: "",
-            departureArrival: "Aktueller Standort",
-            track: "",
-            trailingIcon: Icon(Icons.edit)),
-        SizedBox(
-          height: 20,
-        ),
-        StopoverCard(
-          leadingIcon: Icon(Icons.train),
-          startStation: "Gera Hbf",
-          distance: "15km",
-          departureArrival: "08:00 - 08:30",
-          track: "Gleis 5",
-          trailingIcon: Icon(Icons.work),
-        ),
-        StopoverCard(
-          leadingIcon: Icon(Icons.train),
-          startStation: "Saalfeld Hbf",
-          distance: "3km",
-          departureArrival: "08:00 - 08:30",
-          track: "Gleis 5",
-          trailingIcon: Icon(Icons.home),
-        ),
-      ],
-    );
+    return Column();
   }
 }
